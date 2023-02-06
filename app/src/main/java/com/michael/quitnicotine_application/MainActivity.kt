02c91c2b3d4733b4a049612dd0a7eed4c2c90d2b
Karambasss <m.papa.09@mail.ru>
2023-02-06 23:30:53 +0300

    Добавил цветовую тему в приложение + иконку

--- app/src/main/java/com/michael/quitnicotine_application/MainActivity.kt
+++ app/src/main/java/com/michael/quitnicotine_application/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.michael.quitnicotine_application
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
