0acef7362e3b96c91d0d3b3eb66645022093da9e
Karambasss <m.papa.09@mail.ru>
2023-02-08 00:44:07 +0300

    Работа с предобработкой вводимого пользователем текста на страницах авторизации

--- app/src/main/java/com/michael/quitnicotine_application/view/fragments/FragmentAuth1.kt
+++ app/src/main/java/com/michael/quitnicotine_application/view/fragments/FragmentAuth1.kt
@@ -1,17 +1,37 @@
 package com.michael.quitnicotine_application.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.michael.quitnicotine_application.R
 import kotlinx.android.synthetic.main.fragment_auth1.*
+import kotlinx.android.synthetic.main.fragment_auth2.view.*
 
 class FragmentAuth1 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_auth1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_auth1.setOnClickListener {
+            if (checkField()){
+                Log.d("CheckFragment1Text","Name: ${authName.text}")
+            }
+        }
+    }
+
+    private fun checkField() : Boolean {
+        if (authName.text.isBlank()) {
+            authName.requestFocus()
+            authName.error = "Введите ваше имя!"
+            return false
+        }
+        return true
     }
 
     companion object {
--- app/src/main/java/com/michael/quitnicotine_application/view/fragments/FragmentAuth2.kt
+++ app/src/main/java/com/michael/quitnicotine_application/view/fragments/FragmentAuth2.kt
@@ -1,12 +1,15 @@
 package com.michael.quitnicotine_application.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.michael.quitnicotine_application.R
+import kotlinx.android.synthetic.main.fragment_auth1.*
 import kotlinx.android.synthetic.main.fragment_auth2.*
+import kotlinx.android.synthetic.main.fragment_auth2.button_auth1
 
 class FragmentAuth2 : Fragment() {
 
@@ -15,6 +18,29 @@ class FragmentAuth2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_auth2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_auth1.setOnClickListener {
+            if (checkAllFields()) {
+                Log.d("CheckFragment2Text","Num: ${authNumber.text} and Price: ${authPrice.text} rub")
+            }
+        }
+    }
+
+   private fun checkAllFields() : Boolean {
+        if (authNumber.length() == 0){
+            authNumber.requestFocus()
+            authNumber.error = "Введите кол-во сигарет"
+            return false
+        }
+        if (authPrice.length() == 0){
+            authPrice.requestFocus()
+            authPrice.error = "Введите стоимость пачки"
+            return false
+        }
+        return true
     }
 
     companion object {
