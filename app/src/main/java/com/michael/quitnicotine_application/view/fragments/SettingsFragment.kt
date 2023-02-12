847dcf36060643b429634231d3a263557fe36783
Karambasss <m.papa.09@mail.ru>
2023-02-12 17:11:35 +0300

    1) Добавлено окно настроек, содержащая полезные ссылки для связи со мной, а также много другого... 2) Исправлен баг со съезжающей кнопкой. 3) Удалено ненужное окно.

--- app/src/main/java/com/michael/quitnicotine_application/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/michael/quitnicotine_application/view/fragments/SettingsFragment.kt
@@ -1,27 +1,13 @@
 package com.michael.quitnicotine_application.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.michael.quitnicotine_application.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = SettingsFragment()
-    }
 }
