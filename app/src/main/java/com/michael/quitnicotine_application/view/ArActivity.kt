72208ad0baca24083238501abb0cd43b28b727ee
Karambasss <m.papa.09@mail.ru>
2023-03-17 15:28:08 +0300

    Выполнен переход на Ar экран в котором рисуется на камере пользователя визуальная AR модель. Подключены: 1) Модуль для нахождения и рисования сцены, где будет отрисовываться модель. 2) Модуль для отрисовки и вставления модели в сцену.
    Создан новый элемент окна - ArActivity, на котором и располагается фрагмент со всем, что нужно для отображения и работы с AR.
    
    Также, реализована полная логика работы окна ArFragment, являющаяся стартовым окном, отвечающее проверки всех зависимостей, необходимых для корректной работы Ar после нажатия на кнопку перехода на икноку камеры, а также отображающее пользователю текстовые данные (вводные - для понимания пользователем, что нужно сделать) (цитата - для повышения мотивации).
    
    Добавлены разрешения для работы с камерой устройства пользователя.

---
+++ app/src/main/java/com/michael/quitnicotine_application/view/ArActivity.kt
@@ -0,0 +1,61 @@
+package com.michael.quitnicotine_application.view
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import com.michael.quitnicotine_application.R
+
+class ArActivity : AppCompatActivity() {
+    private lateinit var arCam: ArFragment //object of ArFragment Class
+    private var clickNo = 0 //helps to render the 3d model only once when we tap the screen
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar)
+        arCam = (supportFragmentManager.findFragmentById(R.id.arCameraArea) as ArFragment?)!!
+        //ArFragment is linked up with its respective id used in the activity_main.xml
+        arCam.setOnTapArPlaneListener {
+                hitResult: HitResult, plane: Plane?, motionEvent: MotionEvent? ->
+            clickNo++
+            //the 3d model comes to the scene only when clickNo is one that means once
+            if (clickNo == 1) {
+                val anchor = hitResult.createAnchor()
+                ModelRenderable.builder()
+                    .setSource(this, R.raw.gfg_gold_text_stand_2)
+                    .setIsFilamentGltf(true)
+                    .build()
+                    .thenAccept { modelRenderable: ModelRenderable? ->
+                        addModel(
+                            anchor,
+                            modelRenderable
+                        )
+                    }
+                    .exceptionally { throwable: Throwable ->
+                        val builder = AlertDialog.Builder(this)
+                        builder.setMessage("Somthing is not right" + throwable.message).show()
+                        null
+                    }
+            }
+        }
+    }
+    private fun addModel(anchor: Anchor, modelRenderable: ModelRenderable?) {
+        val anchorNode = AnchorNode(anchor)
+        // Creating a AnchorNode with a specific anchor
+        anchorNode.setParent(arCam.arSceneView.scene)
+        //attaching the anchorNode with the ArFragment
+        val model = TransformableNode(arCam.transformationSystem)
+        model.setParent(anchorNode)
+        //attaching the anchorNode with the TransformableNode
+        model.renderable = modelRenderable
+        //attaching the 3d model with the TransformableNode that is already attached with the node
+        model.select()
+    }
+}
--- app/src/main/java/com/michael/quitnicotine_application/view/fragments/ArFragment.kt
+++ app/src/main/java/com/michael/quitnicotine_application/view/fragments/ArFragment.kt
@@ -1,12 +1,20 @@
 package com.michael.quitnicotine_application.view.fragments
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.michael.quitnicotine_application.R
+import com.michael.quitnicotine_application.view.ArActivity
 import kotlinx.android.synthetic.main.fragment_ar.*
+import java.util.*
 import kotlin.random.Random
 
 class ArFragment : Fragment() {
@@ -22,7 +30,10 @@ class ArFragment : Fragment() {
         initData()
 
         openArModelButton.setOnClickListener {
-
+            if (checkSystemSupport(requireActivity())){
+                val intent = Intent(activity, ArActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 
@@ -35,6 +46,33 @@ class ArFragment : Fragment() {
     private fun getRandomQuote(quotesArray : Array<String>) : String{
         val number = Random.nextInt(quotesArray.size - 0)
         return quotesArray[number]
+    }
+
+    private fun checkSystemSupport(activity: Activity): Boolean {
+
+        //checking whether the API version of the running Android >= 24 that means Android Nougat 7.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val openGlVersion = (Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)) as ActivityManager).deviceConfigurationInfo.glEsVersion
+
+            //checking whether the OpenGL version >= 3.0
+            if (openGlVersion.toDouble() >= 3.0) {
+                return true
+            } else {
+                Toast.makeText(
+                    activity,
+                    "App needs OpenGl Version 3.0 or later",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            }
+        } else {
+            Toast.makeText(
+                activity,
+                "App does not support required Build Version",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
     }
 
     companion object {
